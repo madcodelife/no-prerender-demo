2ae012c048c80cb9c9bcdf3786fb67bf4ff248b1
choujiaojiao <gassnake999@163.com>
2019-08-23 11:08:43 +0800

    change router

--- src/router.js
+++ src/router.js
@@ -9,17 +9,18 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
+      path: '/no-prerender-demo',
       name: 'home',
       component: Home
     },
     {
-      path: '/about',
+      path: '/no-prerender-demo/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () =>
+        import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 })
